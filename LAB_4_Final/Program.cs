8e4c6c59ae99c08d7cf1329d8a6c33c9a8b1a910
Afnan Ghoni Brinto <24-60363-3@student.aiub.edu>
2026-03-17 22:44:26 +0600

    Remove unnecessary whitespace and reformat code

--- LAB_4_Final/Program.cs
+++ LAB_4_Final/Program.cs
@@ -13,33 +13,27 @@ namespace LAB_4_Final
         static void Main(string[] args)
         {
             
-                // Account
+                
                 Account a1 = new Account("Rahim", "A1", 5000);
                 Account a2 = new Account("Karim", "A2", 2000);
                 a1.Transfer(1000, a2);
 
-                // Book
                 Book b1 = new Book("C#", "Author", "B1", "Programming", 5);
 
-                // Library
                 Library lib = new Library("City Library", "Dhaka");
                 lib.AddNewBook(b1);
                 lib.ShowLibInfo();
 
-                // Contact
                 Contact c1 = new Contact("Rahim", "C1", 22, "01712345678", 'M');
 
-                // AddressBook
                 AddressBook ab = new AddressBook("My Contacts", "Personal");
                 ab.AddContact(c1);
                 ab.ShowAllContactInfo();
 
-                // Mobile
                 Mobile m = new Mobile("Rahim", "01712345678", 100, "Android", false);
                 m.CallSomeone(10);
                 m.ShowInfo();
 
-                // Course
                 Course course = new Course("OOP", "CS101", 3);
                 course.ShowCourseInfo();
             
